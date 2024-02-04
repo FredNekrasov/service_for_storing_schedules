345cef2ccc31857804c30125716e299ee563bb92
FredNekrasov <frednekrasov49@gmail.com>
2024-02-05 00:34:05 +0300

    adding mappers to services

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 using API_for_mobile_app.model;
+using API_for_mobile_app.model.dto;
+using API_for_mobile_app.model.dto.date;
+using API_for_mobile_app.model.dto.rooms;
+using API_for_mobile_app.model.entities;
+using API_for_mobile_app.model.entities.date;
+using API_for_mobile_app.model.entities.rooms;
+using API_for_mobile_app.model.mappers;
+using API_for_mobile_app.model.mappers.special_entities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +15,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TimetableDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TimetableConnectionString")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IMapSpecialEntities<AudienceDto, Audience>, MapAudience>();
+builder.Services.AddScoped<IMapSpecialEntities<WeekDto, Week>, MapWeek>();
+builder.Services.AddScoped<IMapSpecialEntities<PairDto, Pair>, MapPair>();
+builder.Services.AddScoped<IMapSpecialEntities<DayDto, Day>, MapDay>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
