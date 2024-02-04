8ffaca6022d23cd19320643a5543e5a1cf5572db
FredNekrasov <frednekrasov49@gmail.com>
2024-02-04 21:55:20 +0300

    creating entities that are stored in a database

---
+++ model/entities/date/Day.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_for_mobile_app.model.entities.date
+{
+    [PrimaryKey(nameof(ID))]
+    public class Day
+    {
+        public Guid ID { get; set; }
+        public string DayOfWeek { get; set; } = string.Empty;
+        public Guid? WeekID { get; set; }
+        public Guid? FirstPairID { get; set; }
+        public Guid? SecondPairID { get; set; }
+        public Guid? ThirdPairID { get; set; }
+        public Guid? FourthPairID { get; set; }
+        public Guid? FifthPairID { get; set; }
+        public Guid? SixthPairID { get; set; }
+        public Guid? SeventhPairID { get; set; }
+        public virtual Pair Pair1 { get; set; }
+        public virtual Pair Pair2 { get; set; }
+        public virtual Pair Pair3 { get; set; }
+        public virtual Pair Pair4 { get; set; }
+        public virtual Pair Pair5 { get; set; }
+        public virtual Pair Pair6 { get; set; }
+        public virtual Pair Pair7 { get; set; }
+        public virtual Week Week { get; set; }
+    }
+}
---
+++ model/entities/rooms/AudienceType.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_for_mobile_app.model.entities.rooms
+{
+    [PrimaryKey(nameof(ID))]
+    public class AudienceType
+    {
+        public Guid ID { get; set; }
+        public string TypeName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public virtual ICollection<Audience> Rooms { get; set; }
+    }
+}
