29c96cd918d6ec945278f9dabb0db1693583a38b
FredNekrasov <frednekrasov49@gmail.com>
2024-02-09 20:57:07 +0300

    deleting outdated dtos and entities

--- model/dto/date/SemesterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_for_mobile_app.model.dto.date
-{
-    public class SemesterDto
-    {
-        public Guid ID { get; set; }
-        public int? Year { get; set; }
-        public bool? IsEven { get; set; }
-    }
-}
--- model/dto/date/WeekDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_for_mobile_app.model.dto.date
-{
-    public class WeekDto
-    {
-        public Guid ID { get; set; }
-        public SemesterDto? Semester { get; set; }
-        public int? WeekNumber { get; set; }
-    }
-}
