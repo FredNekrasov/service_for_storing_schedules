c4d5331212d095d0a7286645c0545434e274bc9d
FredNekrasov <frednekrasov49@gmail.com>
2024-02-17 10:39:13 +0300

    refactoring and simplifying the DayDto class

--- Models/dto/date/DayDto.cs
+++ Models/dto/date/DayDto.cs
@@ -1,19 +1,16 @@
-﻿using Web_API_for_scheduling.Models.entities.date;
-using Web_API_for_scheduling.Models.entities;
-
-namespace Web_API_for_scheduling.Models.dto.date
+﻿namespace Web_API_for_scheduling.Models.dto.date
 {
     public class DayDto
     {
         public int ID { get; set; }
         public string DayOfWeek { get; set; } = string.Empty;
-        public PairDto? Pair1 { get; set; }
-        public PairDto? Pair2 { get; set; }
-        public PairDto? Pair3 { get; set; }
-        public PairDto? Pair4 { get; set; }
-        public PairDto? Pair5 { get; set; }
-        public PairDto? Pair6 { get; set; }
-        public PairDto? Pair7 { get; set; }
+        public int? Pair1 { get; set; }
+        public int? Pair2 { get; set; }
+        public int? Pair3 { get; set; }
+        public int? Pair4 { get; set; }
+        public int? Pair5 { get; set; }
+        public int? Pair6 { get; set; }
+        public int? Pair7 { get; set; }
         public WeekDto Week { get; set; }
     }
 }
